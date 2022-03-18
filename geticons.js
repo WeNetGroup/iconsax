097f125d683863c99e3ef29b303a5975013c4e37
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2022-03-18 09:27:53 +0100

    Update geticons.js

--- geticons.js
+++ geticons.js
@@ -8,7 +8,7 @@ function init_iconsax(){
     document.querySelectorAll(".iconsax").forEach(iconsax => {
     	var TuT = iconsax.getAttribute("icon-name").toLowerCase().trim();
     	
-    	fetch("https://github.com/rwawiorka/iconsax/icons/" + TuT + ".svg")
+    	fetch("//rwawiorka.github.io/iconsax/icons/" + TuT + ".svg")
     	.then(n_n => {
     	  return n_n.text();
     	})
